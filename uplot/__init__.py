e94048fc2f2f896f5c5af658d4670f7bf0d8f4f7
Dmitry Makarov <makarovdmv@gmail.com>
2025-04-13 15:30:42 +0300

    [ci] update from setup.py to pyproject.toml

--- uplot/__init__.py
+++ uplot/__init__.py
@@ -1,4 +1,4 @@
-from uplot.info import __version__, __author__, __email__  # noqa: F401
+from importlib.metadata import metadata
 
 # engine managing
 import uplot.engine as engine
@@ -18,9 +18,24 @@ from uplot.utype import LineStyle, MarkerStyle, AspectMode, AxisScale, Colormap
 # settings
 from uplot.default import DEFAULT
 
+# query the package metadata
+meta = metadata(__name__)
+
+__author__ = meta['Author-email']
+__email__ = meta['Author-email']
+__version__ = meta['Version']
+__license__ = meta['License-Expression']
+
 
 __all__ = [
-    
+
+    # package metadata
+
+    "__version__",
+    "__author__",
+    "__email__",
+    "__license__",
+
     # modules
 
     'engine',
@@ -28,19 +43,19 @@ __all__ = [
 
     # interface
 
-    'IFigure', 
+    'IFigure',
     'IPlotEngine',
-    
+
     # functions
-    
+
     'figure',
 
     # types
-   
-    'LineStyle', 
-    'MarkerStyle', 
+
+    'LineStyle',
+    'MarkerStyle',
     'AspectMode',
-    'AxisScale', 
+    'AxisScale',
     'Colormap',
 
     # variables / constants
